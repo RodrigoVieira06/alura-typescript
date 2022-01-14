d2b90fc3a0460a58433461e90f2170328886991f
RodrigoVieira06 <rodrigovlima06@gmail.com>
2022-01-13 21:43:03 -0300

    Curso TS1 Alura - Aula 2 - Adicionada a configuração do Typescript e seu compilador.

--- dist/js/app.js
+++ dist/js/app.js
@@ -1,9 +1,2 @@
-import { Negociacao } from "./models/Negociacao.js";
-
+import { Negociacao } from "./models/negociacao.js";
 const negociacao = new Negociacao(new Date, 10, 100);
-
-console.log(negociacao);
-console.log(negociacao.data);
-console.log(negociacao.quantidade);
-console.log(negociacao.valor);
-console.log(negociacao.volume);
